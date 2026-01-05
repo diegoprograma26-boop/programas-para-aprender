7970f031963b49e3b6758854e177f4cfc82a9dd9
diegoprograma26-boop <diegoprograma26@gmail.com>
2026-01-05 00:47:32 -0600

    agregre dinokombat

--- dinokombat/dinokombat.py
+++ dinokombat/dinokombat.py
@@ -1,1 +1,26 @@
 print ("DINOKOMBAT")
+def interfaz():
+    while (True):
+        menu = ["Bienvenido a  DINOKOMBAT", "1- Kombate", "2- Crear dinosaurio", "3- Dinosaurios creados", "4- Instrucciones", "5- salir"]
+        for menus in menu:
+            print(f" {menus}")
+        kombate = ("Bienvenido al kombate")
+        try:
+            eleccion = (int(input("Que te gutaria hacer: ")))
+            if eleccion == 1:
+                print (kombate)
+            elif eleccion == 2:
+                print ("hola mi bro")
+            elif eleccion == 3:
+                print ("hola carnal")
+            elif eleccion == 4:
+                print ("estas son las intrucciones")
+            elif eleccion == 5:
+                print ("Byeeee")
+            else:
+                print("no es una opcion")
+                return interfaz()
+            break
+        except ValueError:
+                print("no es una opcion")
+interfaz()
